7aff22ba1708ceb50f48ae31e9b3b8771888ac0e
Elperson <40611324+Elpersonn@users.noreply.github.com>
2021-12-02 17:53:54 +0100

    Create dive_1.lua

---
+++ day-2/dive_1.lua
@@ -0,0 +1,19 @@
+function split (inputstr, sep)
+        local t={}
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                table.insert(t, str)
+        end
+        return t
+end
+local dict = {["up"] = -1, ["down"] = 1}
+local tbl = split(io.read("*all"), "\n")
+local x, y = 0, 0
+for i,v in pairs(tbl) do
+  local cmd = split(v, "%s")
+  if cmd[1] == "forward" then
+    x = x + cmd[2]
+  else
+    y = y + cmd[2] * dict[cmd[1]]
+  end
+end
+print(x*y)
