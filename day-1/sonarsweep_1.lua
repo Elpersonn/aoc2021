a5dd71b7e91874e0f704cd329321e8a26b923650
Elperson <40611324+Elpersonn@users.noreply.github.com>
2021-12-02 17:57:23 +0100

    remove unncesarry code

--- day-1/sonarsweep_1.lua
+++ day-1/sonarsweep_1.lua
@@ -9,7 +9,7 @@ end
 local tbl = split(io.read("*all"), "\n")
 local bigger = 0
 for i = 2, #tbl do
-    if tbl[i] > (tbl[i - 1] or 0) then
+    if tbl[i] > tbl[i - 1] then
        bigger = bigger + 1
     end
 
